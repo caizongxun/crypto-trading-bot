2d08c908317b50e3dc49eae1fe7f92cf90b812fc
zong <69517696+caizongxun@users.noreply.github.com>
2026-01-13 16:50:28 +0800

    Initialize modules package

---
+++ modules/__init__.py
@@ -0,0 +1,5 @@
+from .indicators import CompositeIndicator
+from .feature_engineering import FeatureEngineer
+from .model import TradingModel
+
+__all__ = ['CompositeIndicator', 'FeatureEngineer', 'TradingModel']
